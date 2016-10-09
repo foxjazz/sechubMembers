051a1c997ddad9e6490e96abcbefdea775b69ca0
foxjazz <foxr4@foxjazz.net>
2016-10-09 09:14:24 -0600

    safe keeping

--- src/app/Login/config.ts
+++ src/app/Login/config.ts
@@ -4,6 +4,9 @@
 
 export let config = {
     test: 'http://localhost:3000',
+    core: 'http://localhost:51189/api/Login',
     prod: 'http://74.208.129.62:3000'
 
 };
+
+
--- src/app/Login/login.service.ts
+++ src/app/Login/login.service.ts
@@ -3,7 +3,7 @@
  */
 import { Injectable } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import {  Http, Response } from '@angular/http';
+import {  Headers, RequestOptions, Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { config } from './config';
 import 'rxjs/add/operator/map';
@@ -12,25 +12,32 @@ import 'rxjs/add/operator/map';
 export class LoginService {
     private uri: string;
     private http: Http;
+    private data: any;
+    private handleError: any;
     constructor (private h: Http) {
-        this.uri = config.test;
+        this.uri = config.core;
         this.http = h;
     }
 
-    public login (loginData: NgForm): boolean
+    public login(loginData: NgForm): boolean
     {
-        // let result: Observable<Response>;
-        let uriplus = this.uri + '/login';
-        // result =  this.http.post(uriplus, loginData);
-
+        this.login2(loginData).subscribe(test => this.data = test);
         return true;
+    }
+    public login2 (loginData: NgForm): Observable<any>
+    {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions( { headers: headers } );
+        let uriplus = this.uri; // + '/login';
+        return this.http.post(uriplus, loginData, options)
+            .map(this.data);
+
     }
 
     public register (registerData: NgForm): boolean
     {
-        // let result: Observable<Response>;
         let uriplus = this.uri + '/register';
-        // result =  this.http.post(uriplus, registerData);
+        this.http.post(uriplus, registerData);
         return true;
     }
 }
