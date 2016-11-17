4bf6aed7e70d0fde677b19a75a537ca080a0b65f
foxjazz <foxr4@foxjazz.net>
2016-11-17 15:33:25 -0700

    s

---
+++ src/app/regions/region.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RegionComponent } from './region.component';
+import { HttpModule } from '@angular/http';
+
+
+@NgModule({
+    declarations: [
+        RegionComponent
+    ],
+    imports: [ HttpModule ],
+    exports: [
+        RegionComponent
+    ]
+})
+export class RegionModule {
+}
+
+/**
+ * Created by fox21 on 11/16/2016.
+ */
+/**
+ * Created by fox21 on 11/16/2016.
+ */
---
+++ src/app/regions/region.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+import { RegionComponent } from './Region.component';
+
+export const RegionRoutes: Routes = [
+    { path: 'regionroutes',  component: RegionComponent }
+];
+/**
+ * Created by fox21 on 11/16/2016.
+ */
+/**
+ * Created by fox21 on 11/16/2016.
+ */
